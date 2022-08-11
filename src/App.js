7863dfa0975badda5194fdc5dfe19a622a7aa792
6310110374 <6310110374@psu.ac.th>
2022-08-12 00:59:04 +0700

    part4

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
+
+const word = "Hello";
+function App() {
+  return (
+    <div>
+      {
+        <WordCard value = {word}/>
+      }
+    </div>
+  );
+}
+export default App;
+ 
